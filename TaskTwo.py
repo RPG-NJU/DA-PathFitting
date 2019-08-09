fb292c9d364b1c6db131085354d6dd1d0c364ba8
HELLORPG <hellorpg2017@gmail.com>
2019-08-09 11:47:59 +0800

    [fix] 重构了一下函数的架构

--- TaskTwo.py
+++ TaskTwo.py
@@ -39,13 +39,93 @@ def GetSpeed(coord: list) -> float:
     return numpy.mean(speed_list)
 
 
-def test(coord: list):
+def LinearInter(coord: list, mode=0) -> list:
     """
-    :param coord: coord原始格式数据
+    :param coord:
+    :param mode: 插值的模式
+    :return: 返回插值的数据
+    """
+    X = list()
+    Y = list()
+    if mode == 0:
+        """此时是最简单的内插完成"""
+        x1 = coord[0][1]
+        y1 = coord[0][2]
+        t1 = coord[0][0]
+        x2 = coord[-1][1]
+        y2 = coord[-1][2]
+        t2 = coord[-1][0]
+    else:
+        x1 = coord[0][1]
+        y1 = coord[0][2]
+        t1 = coord[0][0]
+        x2 = coord[-1][1]
+        y2 = coord[-1][2]
+        t2 = coord[-1][0]
+
+    for i in range(1, len(coord) - 1):
+        t = coord[i][0]
+        x = x1 + ((t - t1) / (t2 - t1)) * (x2 - x1)
+        y = y1 + ((t - t1) / (t2 - t1)) * (y2 - y1)
+        X.append(x)
+        Y.append(y)
+
+    return [X, Y]
+
+
+def Linear(coord: list):
+    """
+    :param coord:
     :return:
     """
+    X = list()
+    Y = list()
+
+    for i in range(0, len(coord) - 1):
+        if coord[i + 1][3] == 1 or coord[i][3] == 0:
+            continue
+
+        for j in range(i + 1, len(coord)):
+            if coord[j][3] == 1:
+                XY = LinearInter(coord[i: j + 1])
+                X += XY[0]
+                Y += (XY[1])
+                # i = j
+                break
+
+    # 此时应该开始绘图
     XY = GetAllUsableXY(coord)
-    # print(GetAllUsableXY(coord))
-    GetScatterPlot(XY[0], XY[1])
-    speed = GetSpeed(coord)
-    print(speed)
+    plt.axis('equal')  # 保证XY轴的单位长度是一致的
+    plt.scatter(XY[0], XY[1], alpha=0.6, s=0.4)
+    plt.scatter(X, Y, alpha=0.6, s=0.4, c="r")
+    plt.title("Linear Inter Data")
+    """x y为点的坐标序列 alpha为颜色的深浅 s为点的大小"""
+    plt.show()
+
+
+# def test(coord: list):
+#     """
+#     :param coord: coord原始格式数据
+#     :return:
+#     """
+#     XY = GetAllUsableXY(coord)
+#     # print(GetAllUsableXY(coord))
+#     GetScatterPlot(XY[0], XY[1])
+#     speed = GetSpeed(coord)
+#     print(speed)
+#     Linear(coord)
+
+
+def ShowRawData(coord: list):
+    """
+    :param coord:
+    :return:
+    绘制原始数据的图表
+    """
+    XY = GetAllUsableXY(coord)
+    plt.axis('equal')  # 保证XY轴的单位长度是一致的
+    plt.scatter(XY[0], XY[1], alpha=0.6, s=0.8, c="b")
+    plt.title("Coord Raw Data")
+    """x y为点的坐标序列 alpha为颜色的深浅 s为点的大小"""
+
+    plt.show()
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from ReadFile import *
 from FixData import *
 from DrawPicture import *
 import StandardFix
-from TaskOne import *
-from TaskTwo import *
+import TaskOne
+import TaskTwo
 
 
 def main():
@@ -15,16 +15,19 @@ def main():
     XY_list = GetXYList(path_chan)
     # GetScatterPlot(XY_list[0], XY_list[1])
     # StandardFix.standardFix(XY_list[0], XY_list[1])
-    """这里通过库函数来进行拟合，但是拟合的结果并不是非常符合题意，之后也许会进行优化
+    """
+       这里通过库函数来进行拟合，但是拟合的结果并不是非常符合题意，之后也许会进行优化
        2019.8.2
     """
     # StandardFix.standardFix()
-    # Linear(XY_list[0], XY_list[1])  # 这是进行线性拟合的函数
-    # LinearAndQuadratic(XY_list[0], XY_list[1])
+    # TaskOne.Linear(XY_list[0], XY_list[1])  # 这是进行线性拟合的函数
+    # TaskOne.LinearAndQuadratic(XY_list[0], XY_list[1])
 
     coord = GetCoord(data)
-    print(coord)
-    test(coord)
+    # print(coord)
+    TaskTwo.ShowRawData(coord)
+    # TaskTwo.test(coord)
+    TaskTwo.Linear(coord)
 
 
 main()
